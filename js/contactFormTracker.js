9efcbdc563226bc2e110a3f3eb660404cfe2c30d
Jorge Ferreiro <me@jgferreiro.com>
2015-08-03 07:07:01 +0200

    Clearing session storage when form is sent

--- js/contactFormTracker.js
+++ js/contactFormTracker.js
@@ -1,18 +1,37 @@
 var st = sessionStorage;
 var keepValueFields = document.getElementsByClassName('keepInputValue'); // Every element with the tag "keep value field" will be save/load.
+var form = document.getElementsByClassName('contactForm'); // Every element with the tag "keep value field" will be save/load.
 var saveTime = 1000;	// Time in miliseconds for saving user text input on session storage
 var sessionStorageName = 'contactForm_';
 	
 
 	loadUserText(); // Calling the function the first time we open the page
 	setInterval(saveUserText, saveTime); // Saving user inputs text each "saveTime" miliseconds
+	form.onsubmit = function() { clearFormData; };
 
+	// Clear data when the user send a form
+
+	function clearFormData() {
+		var stkey, stValue, total = keepValueFields.length;
+		
+		// Traversing all the session storage elements and 
+		// clearing the current value for our inputs files.
+
+		for (i = 0; i < total; i++) {
+			stkey 	= sessionStorageName + i; 		// Setting the key name. Change this for a different name 
+			st.setItem(stkey, '');					// Clearing session storage for that name
+		}
+ 
+	}
 
 	// Load values to the inputs field.
 	// We load all the values when the user access this page for the firts time.
 
 	function loadUserText() {
 		var stkey, total = keepValueFields.length;
+
+	 	// Traversing all the session storage elements and 
+	 	// setting the session storage value to our inputs files.
 
 		for (i = 0; i < total; i++) {
 			stkey 	= sessionStorageName + i; 			// Setting the key name. Change this for a different name 
