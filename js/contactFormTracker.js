845b0d896812154cdddae64f4f48d721817e167d
Jor. <me@jgferreiro.com>
2015-10-11 14:17:59 +0200

    Documenting some parts of the code.

--- js/contactFormTracker.js
+++ js/contactFormTracker.js
@@ -1,4 +1,4 @@
-var st = sessionStorage;
+var st = sessionStorage; // use st variable to access and save values to session storage.
 var keepValueFields = document.getElementsByClassName('keepInputValue'); // Every element with the tag "keep value field" will be save/load.
 var form = document.getElementsByClassName('contactForm'); // Every element with the tag "keep value field" will be save/load.
 var saveTime = 1000;	// Time in miliseconds for saving user text input on session storage
@@ -7,23 +7,23 @@ var sessionStorageName = 'contactForm_';
 
 	loadUserText(); // Calling the function the first time we open the page
 	setInterval(saveUserText, saveTime); // Saving user inputs text each "saveTime" miliseconds
-	form.onsubmit = function() { clearFormData; };
+	form.onsubmit = function() { clearFormData; }; // Clear data when form is sent.
 
 	// Clear data when the user send a form
-
+	
 	function clearFormData() {
-		var stkey, stValue, total = keepValueFields.length;
+		var stkey, stValue, total = keepValueFields.length; // total is the number of inputs we're tracking
 		
 		// Traversing all the session storage elements and 
 		// clearing the current value for our inputs files.
 
 		for (i = 0; i < total; i++) {
-			stkey 	= sessionStorageName + i; 		// Setting the key name. Change this for a different name 
-			st.setItem(stkey, '');					// Clearing session storage for that name
+			stkey 	= sessionStorageName + i;  // get session storage key from the web form
+			st.setItem(stkey, '');	// Clearing data for that specific key.
 		}
 	}
 
-	// Load values to the inputs field.
+	// Set values to inputs field any time this function is called.
 	// We load all the values when the user access this page for the firts time.
 
 	function loadUserText() {
